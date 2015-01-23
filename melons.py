7219a7ae7dd8e8c9220b3c6cd0388c9bcba2d320
Hackbright Student <info@hackbrightacademy.com>
2015-01-22 18:38:03 -0800

    class hierarchy test case: melon>SqMelon>AbstractMelon. inherited vs. modified attributes.

--- melons.py
+++ melons.py
@@ -1,23 +1,45 @@
 """This file should have our melon-type classes in it."""
 
 class AbstractMelon(object):
-    def __init__(self,my_type,my_shape="natural",origin=False):
-        self.melon_type = my_type
+    BASE_MELON_PRICE = 5.0
+    def __init__(self,melon_type):
+        self.melon_type = melon_type
         self.color = None
-        self.imported = origin
-        self.shape = my_shape
+        self.imported = False
+        self.shape = "natural"
         self.seasons = None
+        self.pricetag = self.BASE_MELON_PRICE
+    def get_price(self,qty,pricetag=BASE_MELON_PRICE):
+        return pricetag*qty
 
 class SquareMelon(AbstractMelon):
-    def __init__(self, my_type):
-        return super(SquareMelon,self).__init__(my_type,my_shape="square")
+    def __init__(self, melon_type):
+        super(SquareMelon,self).__init__(melon_type)
+        # reset attributes for a square melon
+        self.shape = "square"
+        self.pricetag = self.BASE_MELON_PRICE * 2
+    def get_price(self,qty):
+        return super(SquareMelon,self).get_price(qty,self.pricetag)
 
 class ImportedMelon(AbstractMelon):
-    def __init__(self, my_type):
-        return super(ImportedMelon,self).__init__(my_type,origin=True)
+    def __init__(self, melon_type):
+        super(ImportedMelon,self).__init__(melon_type)
+        # reset attributes for an imported melon
+        self.imported = True   
+        self.pricetag = self.BASE_MELON_PRICE * 1.5
+    def get_price(self,qty):
+        return super(ImportedMelon,self).get_price(qty,self.pricetag)
+
+class Xigua(SquareMelon):
+    def __init__(self, melon_type="Xigua"):
+        super(Xigua,self).__init__(melon_type)
+        # Xigua is square and imported. Reset price tag.
+        self.pricetag = self.BASE_MELON_PRICE * 2 * 1.5
+        # Xigua has other specific attributes of color and seasons.
+        self.color = "black"
+        self.seasons = ['Summer']
+
+## TODO:  separate class per each type of melon?
+## TODO:  how can do dual-class?  imported and square?
 
 
-
-# class Xigua(SquareMelon):
-#     def __init__(self):
-#         super(Xigua,self).__init__(my_type)
