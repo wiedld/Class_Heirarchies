1e0c5ae95597e034908681933802d2da669ac26d
wiedld <dlw405@gmail.com>
2015-01-26 22:21:40 -0800

    can use multiple hierarchies

--- melons.py
+++ melons.py
@@ -17,7 +17,7 @@ class SquareMelon(AbstractMelon):
         super(SquareMelon,self).__init__(melon_type)
         # reset attributes for a square melon
         self.shape = "square"
-        self.pricetag = self.BASE_MELON_PRICE * 2
+        self.pricetag = self.pricetag * 2   # 10
     def get_price(self,qty):
         return super(SquareMelon,self).get_price(qty,self.pricetag)
 
@@ -26,7 +26,7 @@ class ImportedMelon(AbstractMelon):
         super(ImportedMelon,self).__init__(melon_type)
         # reset attributes for an imported melon
         self.imported = True   
-        self.pricetag = self.BASE_MELON_PRICE * 1.5
+        self.pricetag = self.pricetag * 1.5     # 7.5
     def get_price(self,qty):
         return super(ImportedMelon,self).get_price(qty,self.pricetag)
 
@@ -34,7 +34,16 @@ class Xigua(SquareMelon):
     def __init__(self, melon_type="Xigua"):
         super(Xigua,self).__init__(melon_type)
         # Xigua is square and imported. Reset price tag.
-        self.pricetag = self.BASE_MELON_PRICE * 2 * 1.5
+        self.pricetag = self.pricetag * 1.5     # 15
+        # Xigua has other specific attributes of color and seasons.
+        self.color = "black"
+        self.seasons = ['Summer']
+
+class Xigua2(SquareMelon,ImportedMelon):
+    def __init__(self, melon_type="Xigua"):
+        super(Xigua2,self).__init__(melon_type)
+        # the two class, with the self.pricetag=self.pricetag * modifier
+            #outcome should be pricetag = 15
         # Xigua has other specific attributes of color and seasons.
         self.color = "black"
         self.seasons = ['Summer']
